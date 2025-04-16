a6383b29911cfee39517a79bc26386b4c8160e02
Jordan Imp <jsi238@nyu.edu>
2025-04-16 13:53:16 -0400

    Sucking and Animations

--- Scripts/ghost_controller.gd
+++ Scripts/ghost_controller.gd
@@ -2,19 +2,24 @@ extends Area2D
 
 var vacuum_large_ref # large area slow suck
 var vacuum_small_ref # small area fast suck
+var animator
 
 var isBigSucked = false
 var isSmallSucked = false
 
 const FAST_SHRINK = .01
 const SLOW_SHRINK = .005
-const SPEED = 50
+const X_SPEED = 40
+
+var rng
+
+var direction = -1 # starts facing the left
 
 func _ready()->void:
+	animator = get_child(1)
+	
 	vacuum_large_ref = get_node("/root/Level/Vacuum").get_child(2)
 	vacuum_small_ref = get_node("/root/Level/Vacuum").get_child(1);
-
-	print(vacuum_large_ref.name + " " + vacuum_small_ref.name)
 	
 func _process(delta: float) -> void:
 	if (vacuum_large_ref.overlaps_area(self)):
@@ -34,3 +39,16 @@ func _process(delta: float) -> void:
 		
 	if self.scale.x <= .5:
 		self.queue_free()
+		
+	if direction == 1:
+		animator.play("drift_right")
+	elif direction == -1:
+		animator.play("drift_left")
+		
+func _physics_process(delta: float) -> void:
+	if (isBigSucked):
+		pass
+	elif (isSmallSucked):
+		pass
+	else:
+		global_position.x += X_SPEED * delta * direction
