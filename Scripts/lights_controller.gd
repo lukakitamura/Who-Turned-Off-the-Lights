5715d18ba12f97ff90831d3eb4dbd1927e998a95
luka <luka.kitamura@gmail.com>
2025-05-09 18:51:46 -0400

    updated light controller to be based on linear pot

--- Scripts/lights_controller.gd
+++ Scripts/lights_controller.gd
@@ -13,6 +13,8 @@ var START_SCREEN
 var timer = 1
 var reset = false
 
+var inputVector = Vector2.ONE 
+
 func _ready()->void:
 	player_ref = get_node("/root/Level/Alan")
 	player_ref.stop_game.connect(_stop_game)
@@ -21,6 +23,7 @@ func _process(delta: float) -> void:
 	
 	if (!reset):
 		if (!stop_game):
+			'''
 			if (Input.is_action_pressed("ui_lights")):
 				self_modulate.a -= INCREASE_RATE / 33.0 * delta
 				self_modulate.a = clamp(self_modulate.a, 0, 1)
@@ -29,6 +32,8 @@ func _process(delta: float) -> void:
 				self_modulate.a += DECAY_RATE / 75.0 * delta
 				self_modulate.a = clamp(self_modulate.a, 0, 1)
 				# print("Screen is getting darker")
+			'''
+			self_modulate.a = get_input_vector().x
 		else:
 			self_modulate.a = 0 # turn on lights when game ends
 		
@@ -36,7 +41,8 @@ func _process(delta: float) -> void:
 		
 		if Input.is_action_pressed("ui_quit"):
 			reset = true
-	
+		#print(get_input_vector().x)
+		print(Input.get_connected_joypads())
 		# print(self_modulate.a)
 	else:
 		self_modulate.a = 1 #turn lights off if player gives up
@@ -47,3 +53,7 @@ func _process(delta: float) -> void:
 
 func _stop_game()->void:
 	stop_game = true
+
+func get_input_vector():
+	inputVector.x = remap(Input.get_action_strength("light_up") - Input.get_action_strength("light_down"), -1, 1, 1, 0)
+	return inputVector 
